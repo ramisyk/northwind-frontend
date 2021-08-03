6ea85e685e8f0f195cf29c2bb29699319221aef1
ramisyk <ramisyk@RAMISYK>
2021-08-04 00:19:13 +0300

    add get request with http client

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
@@ -13,10 +15,20 @@ export class ProductComponent implements OnInit {
   products:Product[] = [
    
   ];
-  
-  constructor() { }
+  apiUrl = "https://localhost:44314/api/products/getall"
+ 
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.httpClient
+    .get<ProductResponseModel>(this.apiUrl)
+    .subscribe(response => {
+      this.products = response.data
+    });
   }
 
 }
